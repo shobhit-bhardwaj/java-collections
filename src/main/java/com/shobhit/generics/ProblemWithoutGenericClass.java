2b978b715078dc2dd03321f4fa645efc51627fd4
Shobhit Bhardwaj <successshobhit@gmail.com>
2023-11-14 14:38:27 +0530

    Java Collection Framework

---
+++ src/main/java/com/shobhit/generics/ProblemWithoutGenericClass.java
@@ -0,0 +1,24 @@
+package com.shobhit.generics;
+
+public class ProblemWithoutGenericClass {
+	private static class SimpleClass {
+		private Object value;
+
+		public SimpleClass(Object value) {
+			this.value = value;
+		}
+
+		public Object getValue() {
+			return value;
+		}
+	}
+
+	public static void main(String[] args) {
+		SimpleClass integerObject = new SimpleClass(15);
+		SimpleClass stringObject = new SimpleClass("String Value");
+
+		integerObject = stringObject;	//	Doesn't Make Sence but Code will Compile
+
+		//String value = stringObject.getValue();	//	Make Sence but Code won't Compile
+	}
+}
