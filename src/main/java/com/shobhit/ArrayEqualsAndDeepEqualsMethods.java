6c2f0086b03ce563ef303e483e52f90aeec5baf6
Shobhit Bhardwaj <successshobhit@gmail.com>
2023-11-14 14:54:37 +0530

    Adding Tab

--- src/main/java/com/shobhit/ArrayEqualsAndDeepEqualsMethods.java
+++ src/main/java/com/shobhit/ArrayEqualsAndDeepEqualsMethods.java
@@ -10,7 +10,7 @@ public class ArrayEqualsAndDeepEqualsMethods {
 		Integer[] array2 = new Integer[] {1, 2, 3, 4, 5};
 		System.out.println("Array 2 - " + Arrays.toString(array2));
 
-		System.out.println("Arrays equals() - " + Arrays.equals(array1, array2));			//	Works on Primitive and Object Array - int[]
+		System.out.println("Arrays equals() - " + Arrays.equals(array1, array2));				//	Works on Primitive and Object Array - int[]
 		System.out.println("Arrays deepEquals() - " + Arrays.deepEquals(array1, array2));		//	Works on Object Array Only - Integer[]
 		System.out.println();
 
